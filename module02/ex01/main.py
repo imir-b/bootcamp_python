1d1ad26275aaad62be8a9645f1afb5a9d2e99c0e
vbleskin <vbleskin@f2r11s7.paris.42.school>
2026-07-13 13:55:55 +0200

    module01 finish

---
+++ module02/ex01/main.py
@@ -0,0 +1,57 @@
+def what_are_the_vars(*args, **kwargs):
+    """
+    Takes a variable number of positional and keyword arguments, 
+    maps them to an ObjectC instance, and handles collisions.
+    """
+    obj = ObjectC()
+    
+    # Handle positional arguments
+    for i, var in enumerate(args):
+        name = f"var_{i}"
+        setattr(obj, name, var)
+        
+    # Handle keyword arguments and check for collisions
+    for key, value in kwargs.items():
+        if hasattr(obj, key):
+            return None
+        setattr(obj, key, value)
+        
+    return obj
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    
+    obj = what_are_the_vars(None, [])
+    doom_printer(obj)
+    
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    
+    # This will print ERROR because positional 42 is var_0, colliding with kwarg var_0
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
+    
+    obj = what_are_the_vars(42, "Yes", a=10, var_2="world")
+    doom_printer(obj)
